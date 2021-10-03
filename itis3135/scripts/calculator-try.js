013f7d1516442edacfe43a1faaaa1bb3d3083b7d
bking54 <78242263+bking54@users.noreply.github.com>
2021-10-03 14:54:56 -0400

    calculator assignment

--- itis3135/scripts/calculator-try.js
+++ itis3135/scripts/calculator-try.js
@@ -21,7 +21,27 @@ function doCalculation(num1, op, num2) {
     return result;
 }
 
+function getState() {
+    if (value1 == undefined && operator == undefined && value2 == undefined) {
+        return 0;
+    } else if (value1 != undefined && operator != undefined && value2 == undefined) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+function clearDisplay() {
+    document.getElementById("calculator-display").innerText = "0";
+    value1 = undefined;
+    operator = undefined;
+    value2 = undefined;
+}
+
 function calculatorInput(input) {
+    let currentDisplay = document.getElementById("calculator-display").innerText;
+    let empty = currentDisplay == "0" || currentDisplay == "+" || currentDisplay == "-" || currentDisplay == "*" || currentDisplay == "/" || currentDisplay == "Infinity";
+    let state = getState();
     switch (input) {
         case 0:
         case 1:
@@ -33,13 +53,51 @@ function calculatorInput(input) {
         case 7:
         case 8:
         case 9:
+            if (empty) {
+                document.getElementById("calculator-display").innerText = input;
+            } else {
+                document.getElementById("calculator-display").innerText += input;
+            }
+            break;
+        case ".":
+            if ((currentDisplay == "0" || !empty) && !currentDisplay.includes(".")) {
+                document.getElementById("calculator-display").innerText += input;
+            }
             break;
         case "+":
         case "-":
         case "*":
         case "/":
+            if (state == 0) {
+                value1 = parseFloat(document.getElementById("calculator-display").innerText);
+                operator = input;
+                document.getElementById("calculator-display").innerText = input;
+            } else if (state == 1) {
+                value2 = parseFloat(document.getElementById("calculator-display").innerText);
+                let temp = doCalculation(value1, operator, value2);
+                clearDisplay();
+                value1 = temp;
+                operator = input;
+                value2 = undefined;
+                document.getElementById("calculator-display").innerText = input;
+            } else {
+                alert("error with operator: " + input);
+            }
             break;
         case "=":
+            if (state == 1 && !empty) {
+                value2 = parseFloat(document.getElementById("calculator-display").innerText);
+                let temp = doCalculation(value1, operator, value2);
+                clearDisplay();
+                document.getElementById("calculator-display").innerText = temp;
+            } else if (state == 1 && empty) {
+                value2 = 0;
+                let temp = doCalculation(value1, operator, value2);
+                clearDisplay();
+                document.getElementById("calculator-display").innerText = temp;
+            } else if (state != 0) {
+                alert("error with equals operator");
+            }
             break;
     }
     return input;
