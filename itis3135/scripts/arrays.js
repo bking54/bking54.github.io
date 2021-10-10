14547c002885eb154b57c139e4b2767055f4e0cd
bking54 <78242263+bking54@users.noreply.github.com>
2021-10-10 00:06:38 -0400

    finish activity 8

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -10,12 +10,14 @@ function displayResults() {
     for (let n = 0; n < salaries.length; n++) sum += salaries[n];
     avg = (sum / salaries.length).toFixed(2);
 
-    let result = "<h3>Results:</h3><p>Average Salary: $" + avg + "</p><p>Highest Salary: $" + max + "</p>";
+    let employeeIndex = parseInt(document.getElementById("arrays-dropdown").value);
+
+    let result = "<h3>Results:</h3><p>Average Salary: $" + avg + "</p><p>Highest Salary: $" + max + "</p><p>" + employees[employeeIndex] + "'s Salary: $" + salaries[employeeIndex] + "</p>";
     document.getElementById("results").innerHTML = result;
 }
 
 function displaySalary() {
-    let result = "<table><thead><tr><th>Employee Name</th><th>Employee Salary</th></tr></thead><tbody>";
+    let result = "<br><table><thead><tr><th>Employee Name</th><th>Employee Salary</th></tr></thead><tbody>";
     for (let n = 0; n < employees.length; n++) {
         result += ("<tr><td>" + employees[n] + "</td><td>$" + salaries[n] + "</td></tr>");
     }
@@ -25,10 +27,28 @@ function displaySalary() {
 
 function addSalary() {
     let newEmployee = document.getElementById("employee").value;
-    let newSalary = document.getElementById("salary").value;
-    document.getElementById("results").innerHTML = "";
+    let newSalary = parseFloat(document.getElementById("salary").value).toFixed(2);
+
+    if (newEmployee == "" && isNaN(newSalary)) {
+        alert("Error: Invalid employee name and salary");
+        document.getElementById("salary").value = "";
+    } else if (newEmployee == "") {
+        alert("Error: No employee name given");
+    } else if (isNaN(newSalary)) {
+        alert("Error: Given salary not a number");
+        document.getElementById("salary").value = "";
+    } else {
+        document.getElementById("results").innerHTML = "<h3>New Employee Entry</h3><p>New Employee: " + newEmployee + "</p><p>Starting Salary: $" + newSalary + "</p";
+        document.getElementById("arrays-dropdown").innerHTML += ("<option value='" + employees.length + "'>" + newEmployee + "</option>");
+        employees[employees.length] = newEmployee;
+        salaries[salaries.length] = newSalary;
+        document.getElementById("salary").value = "";
+        document.getElementById("employee").value = "";
+    }
+
+    moveCursor();
 }
 
 function moveCursor() {
-
+    document.getElementById("employee").focus();
 }
