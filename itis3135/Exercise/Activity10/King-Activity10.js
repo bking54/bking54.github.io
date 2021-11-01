52854063bb7ad2a94198888b4887b0d60663f9dc
bking54 <78242263+bking54@users.noreply.github.com>
2021-11-01 15:11:15 -0400

    small improvements

---
+++ itis3135/Exercise/Activity10/King-Activity10.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+
+        // preload the image for each link
+        $('#image_list a').each(function() {
+                let img = new Image();
+                img.src = $(this).attr('href');
+        });
+
+        // set up the event handlers for each link
+
+    			// get the image URL and caption for each image and animate the caption
+
+            // cancel the default action of each link
+
+
+    // move the focus to the first link
+
+}); // end ready
