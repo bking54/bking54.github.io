a45e316e0b996dbc9c819bb196262a6ed9bb0aeb
bking54 <78242263+bking54@users.noreply.github.com>
2021-10-31 22:12:48 -0400

    contract and index pages

--- itis3135/Exercise/Practice/Practice.js
+++ itis3135/Exercise/Practice/Practice.js
@@ -3,14 +3,31 @@ var $ = function(id) {
 };
 
 window.onload = function() {
-
+	let order = "";
+	let total = 0;
 
 	//add onclick event handler for each image
+	$('espresso').addEventListener("click", addEspresso);
+	$('cappuccino').addEventListener("click", addCappuccino);
 
 	// for click event add item to order and update total
+	function addEspresso() {
+		order = "<p>$1.95 - Espresso - Delicious espresso. Wanna try it?</p>";
+		total += 1.95;
+		displayResults();
+	}
 
+	function addCappuccino() {
+		order = "<p>$3.45 - Cappuccino - Delicious cappuccino!</p>";
+		total += 3.45;
+		displayResults();
+	}
 	// display order and total
-
+	function displayResults() {
+		$('order').innerHTML += order;
+		order = "";
+		$('total').innerText = ("Total: $" + (total).toFixed(2));
+	}
 	
 		
 }; // end onload
