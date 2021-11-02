6a4bffa7f29504866e5e546f0fe715dc6931b631
bking54 <78242263+bking54@users.noreply.github.com>
2021-11-02 14:29:23 -0400

    activity 10 work

--- itis3135/Exercise/Activity10/King-Activity10.js
+++ itis3135/Exercise/Activity10/King-Activity10.js
@@ -11,13 +11,25 @@ $(document).ready(function() {
         $('#image_list a').click(function(evt) {
 
     	        // get the image URL and caption for each image and animate the caption
-                
+                let image_URL = $(this).attr('href');
+                let caption = $(this).attr('title');
+                $('#caption').fadeOut(3000);
+                $('#image').fadeOut(3000, function() {
+                        $('#caption').text(caption);
+                        $('#caption').fadeIn(3000);
+                        $('#caption').animate({fontSize: '2.0em'}, 2000);
+                        $('#image').attr('src', image_URL);
+                        $('#image').fadeIn(3000);
+                });
+
+                //focus on  first link again
+                $('#image_list a').first().focus();
 
                 // cancel the default action of each link
                 evt.preventDefault();
         });
 
-
         // move the focus to the first link
+        $('#image_list a').first().focus();
 
 }); // end ready
